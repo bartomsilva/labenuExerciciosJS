bfb739cb1c11642efc125c2b4915e3f2fa24c42a
bartomsilva <bartomsilva@gmail.com>
2022-11-12 07:40:36 -0300

    feat: exercicio 1 comentado

--- condicionais-II-exercicios-main/exercicio1/script.js
+++ condicionais-II-exercicios-main/exercicio1/script.js
@@ -22,38 +22,60 @@ Faça isso:
 */
 
 
-
-
-
 const number = prompt("digite um número qualquer")
 
-console.log("parte 1")
+// não permite entrada de valores não válidos    
+if ( isNaN(Number(number)) || number===""  || (typeof number !="string") ) {
 
-if ( number%2===0){
-    if (number%3===0){
-        console.log("o número",number,"é divisível por 2 e por 3")
-    } else {
-        console.log("o número",number,"não é divisível por 3, apenas por 2")
+    console.log("você não digitou um número válido!!!")   
+
+    // isNaN = não é possivel convertar para um número válido
+    console.log("---Resultado dos Testes --------")
+    console.log("resultado do isNaN (Not a Number)",isNaN(Number(number))," = ",Number(number) )
+
+    // number===""  = quando não digita nada e clica no ok
+    console.log(`resultado de number ===""`,number==="")
+
+    // typeof number !="string" = quando clica no cancelar retorna um object null
+    console.log(`resultado de typeof number !="string"`,typeof number !="string"," = ", typeof number)
+    console.log("-------------------------------")
+
+} else { 
+
+    console.log("parte 1")
+
+    //1. Utilizando ifs aninhados
+
+    const objDiv = {
+        divPor2: (number%2===0),
+        divPor3: (number%3===0)
     }
-} else {
-    if (number%3===0) { 
-       console.log("o número",number,"não é divisível por 2, apenas por 3")
+
+    if (objDiv.divPor2) {
+        if (objDiv.divPor3) {
+            console.log(`o número ${number} é divisível por 2 e por 3`)
+        } else {
+            console.log(`o número ${number} é divisível apenas por 2`)
+        }
+    } else if (objDiv.divPor3) {
+        console.log(`o número ${number} é divisível apenas por 3`)
     } else {
-        console.log("o número",number,"não é dividido por 2 e 3")
+        console.log(`o número ${number} nãe é divisível por 2 nem por 3`)    
     }
-}
 
-console.log("*".repeat(45))
-console.log("parte 2")
+    console.log("*".repeat(45))
+    console.log("parte 2")
+    //2. Utilizando um operador lógico para unir duas operações relacionais
 
-if (number%2===0 && number%3===0){
-    console.log("o número",number,"é divisível por 2 e por 3")
-} else {
-    if (number%2===0){
-        console.log("o número",number,"não é divisível por 3, apenas por 2")
-    } else if (number%3===0) {
-           console.log("o número",number,"não é divisível por 2, apenas por 3")
+    if (objDiv.divPor2 || objDiv.divPor3){
+        if (objDiv.divPor2 && objDiv.divPor3){        
+            console.log("o número",number,"é divisível por 2 e por 3")
+        } else if (objDiv.divPor2){
+            console.log("o número",number,"é divisível apenas por 2")
+        } else {
+            console.log("o número",number,"é divisível apenas por 3")
+        }
     } else {
-            console.log("o número",number,"não é dividido por 2 e 3")
-    }        
+        console.log("o número",number,"não é divisível por 2 nem por 3")
+    }
 }
