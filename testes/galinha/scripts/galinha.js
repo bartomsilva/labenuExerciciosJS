20d2a2ba2309f13722335a2022a0693e9bb87463
bartomsilva <bartomsilva@gmail.com>
2022-11-13 11:41:35 -0300

    feat: inserido alguns moentarios

--- testes/galinha/scripts/galinha.js
+++ testes/galinha/scripts/galinha.js
@@ -5,17 +5,18 @@
  */
 
    
-const minhasGalinhas = [] 
-const caixaDeOvos = []  
-const imagemGalinha = "./assets/galinha.png"
-let numeroDaGalinha=0
+const minhasGalinhas = []   // lista de galinhas
+const caixaDeOvos = []      // lista de ovos
+const imagemGalinha = "./assets/galinha.png"   // imagem da galinha
+const imagemOvo = "./assets/ovo.png"           // imagem do ovo
+let numeroDaGalinha=0  // numerar as galinhas
 
 
-//função que cria a galinha que pôe ovos
+//função construtora que cria a galinha que pôe ovos
 function Galinha(nomeGalinha) {
-    this.nomeGalinha = nomeGalinha
-    this.numOvo = 0
-    this.totalOvos=0
+    this.nomeGalinha = nomeGalinha   // nomeGalinha: nomeGalinha,
+    this.numOvo = 0                  // numOvo: 0,
+    this.totalOvos=0                 // totalOvos: 0
 
     // método botar ovo, ele invoca uma função que é Ovo
     this.ovoBotar=()=>{
@@ -23,44 +24,61 @@ function Galinha(nomeGalinha) {
         this.totalOvos++
         return new Ovo(this.numOvo,this.nomeGalinha)
     } 
-    // função que cria o ovo e anuncia o feito
+    // função construtora que cria o ovo e anuncia o feito - cria o objeto ovo
     function Ovo(numOvo,minhaGalinha) {
-        this.clara = "65%",
-        this.gema= "35%",
-        this.nomeGalinha=minhaGalinha,
-        this.numOvo=numOvo
-        console.log(`${minhaGalinha} botei o Ovo número ${numOvo} `)
+        this.clara = "65%",             // clara: "65%"
+        this.gema= "35%",               // gema: "35%"
+        this.nomeGalinha=minhaGalinha,  // nomeGalinha: minhaGalinha
+        this.numOvo=numOvo              // numOvo: numOvo 
+        this.nomeDoOvo="ovo"+"-"+numOvo+"-"+minhaGalinha  // nomeDoOvo: "ovo"+"-"+numOvo+"-"+minhaGalinha
     }
 }
 
-const botarOvo=(e)=>{
+// função que manda a gainha por o ovo e é responsável por criar o visual dos ovos
+const botarOvo=(obj)=>{
+    
+    let novaimg = null
+    let novaDiv = document.createElement("div")
     // identifica a galinha
-    const galinha = e.getAttribute("id").substring(1)
+    const galinha = obj.getAttribute("id").substring(1)
     // manda a galinha botar o ovo e poe na caixa
-    caixaDeOvos.push( minhasGalinhas[galinha-1].ovoBotar())
+    const oVo = minhasGalinhas[galinha-1].ovoBotar()
+    const nomeDoOvo="ovo"+"-"+oVo.numOvo+"-"+oVo.nomeGalinha
+
+    caixaDeOvos.push(oVo) // guarda o ovo na caixa
+
+    novaDiv.setAttribute('onclick','criarGalinha(this)')  
+    novaDiv.setAttribute('id',nomeDoOvo)                
+    document.querySelector('.caixaDeOvos').appendChild(novaDiv)
+                    
+    novaimg = document.createElement("img");
+    novaimg.setAttribute('src',imagemOvo)
+    document.querySelector("#"+nomeDoOvo).appendChild(novaimg)
 }
 
-const criarGalinha=()=>{
-    //incrementa o contador de galinhas ( atualziar o Dom )
+const criarGalinha=(origem)=>{
+    //incrementa o contador de galinhas 
     numeroDaGalinha++
 
-    let nomeDaGalinha="galinha"+numeroDaGalinha
-    let novaDiv = document.createElement("div")
+console.log("origem",origem)
+
+    const nomeDaGalinha="g"+numeroDaGalinha
     let novaimg = null
+    let novaDiv = document.createElement("div")
 
     // adiciona a nova galinha 
-    let mgalinha = new Galinha(nomeDaGalinha)
-    minhasGalinhas.push(mgalinha)  
+    let galinha = new Galinha(nomeDaGalinha)
+    minhasGalinhas.push(galinha)  
     
-    novaDiv.setAttribute('class',nomeDaGalinha)   
-    novaDiv.setAttribute('onclick','botarOvo(this)')    
-    novaDiv.setAttribute('id','g'+numeroDaGalinha)                
+    novaDiv.setAttribute('onclick','botarOvo(this)')  
+    novaDiv.setAttribute('id',nomeDaGalinha)                
     document.querySelector('.galinhas').appendChild(novaDiv)
                     
     novaimg = document.createElement("img");
     novaimg.setAttribute('src',imagemGalinha)
-    document.querySelector("#g"+numeroDaGalinha).appendChild(novaimg)
+    document.querySelector("#"+nomeDaGalinha).appendChild(novaimg)
 }
+
 
 //primeira galinha
 criarGalinha()
