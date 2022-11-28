2cdb6466ebe70a3f0b700692ea357ae860cf097e
bartomsilva <bartomsilva@gmail.com>
2022-11-28 12:18:33 -0300

    estudando

--- testes/funções/funcoesArrayPorDentro/pordentro.js
+++ testes/funções/funcoesArrayPorDentro/pordentro.js
@@ -1,25 +1,49 @@
-
 // novo array com os valores multiplicados por 2
 // novo array com os valores divididos por 2
+// novo array com os números pares
+
+const numeros = [1, 2, 3, 4, 5, 6, 8]
+const pessoas =[{name: "Bart", age: 55},{name: "Sandra", age: 48}]
 
 
-const numeros = [1, 2, 3, 4, 5, 6]
-
-function myMap(arr, func) {
-    const newA= []
+function myFuncArr(arr, fCb) {
+    const array1 = []
+    let result=""
     for (const index in arr) {
-        newA.push(func(arr[index]))      
+        result=fCb(arr[index])
+        if(typeof result=="boolean" && result){
+            result=arr[index] 
+        }else if (typeof result=="boolean")  {
+            result=null
+        }
+        typeof result!="undefined"&&result?array1.push(result):""
     }
-    return newA
+    return array1
 }
 
 
+const soPares=(e)=> {
+   if (e%2===0){
+      return e
+   }
+}
+
+const mult2=(e)=> {
+     return e * 2 
+}
+
+//map()
+let ex1 = myFuncArr( numeros, (e) => e * 2 )
+ex1=myFuncArr( pessoas, (e) => e.age /2 )
+console.log(ex1)
 
 
-console.log("map js",numeros.map( e => e * 2)
+//filter()
+ex1 = myFuncArr(numeros,e => e%2===0)
 
+//find()
+ex1 = myFuncArr(numeros, e => e === 8 )
 
-console.log("meu map",myMap(numeros, e => e * 2))
 
 
 
