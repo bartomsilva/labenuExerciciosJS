32f5d47991baff2c24b62a4322ede009eb4ac410
bartomsilva <bartomsilva@gmail.com>
2022-11-29 12:03:48 -0300

    feat: blackjack concluído

--- revisão JS/Blackjack/desafio.js
+++ revisão JS/Blackjack/desafio.js
@@ -1,23 +1,18 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+// declaração das variáveis
+let usLetter = []
+let pcLetter = []
+let usPoints = []
+let pcPoints = []
 
-const usLetter = ["", ""]
-const pcLetter = ["", ""]
-let usPoints = [0]
-let pcPoints = [0]
+while (true) {
+   const resp = confirm("Olá, esse é BLACKJACK do Bart" +
+      "\nDeseja iniciar uma nova rodada?")
+   if (!resp) break
 
-if (confirm("Seja bem-vindo(a) ao jogo de blackjack!\nDeseja iniciar uma nova rodada?")) {
-
+   // resetando cartas e pontos
+   resetGame()
+   
+   // sorteio das cartas iniciais 
    while (true) {
 
       usLetter[0] = comprarCarta()
@@ -29,55 +24,93 @@ if (confirm("Seja bem-vindo(a) ao jogo de blackjack!\nDeseja iniciar uma nova ro
       if (usLetter[0].texto + usLetter[1].texto != "AA"
          && pcLetter[0].texto + pcLetter[1].texto != "AA")
          break
-
    }
-   // compra de cartas do usuário
+
+   // compra de cartas adicionais (usuário)
    while (true) {
-      const resp = confirm("Suas carta são:" +" "+letters(usLetter) + "." +
-         " A carta revelada do computdor é " + pcLetter[0].texto) +
-         "\nDeseja comprar mais uma carta?"
 
-      if (!resp) break
+      // exibe as cartas e a carta revelada do computador
+      // e pergunta se deseja comprar mais cartas
+      const resp = confirm("Suas cartas são:" + " " + letters(usLetter) + "." +
+         "\n A carta revelada do computdor é " + pcLetter[0].texto +
+         "\nDeseja comprar mais uma carta?")
 
+      // se a resposta da pergunta acima for não sai do loop
+      if (!resp) {
+         usPoints = sumPoint(usLetter)
+         break
+      }
+      // uma carta comprada
       usLetter.push(comprarCarta())
+
+      // soma dos pontos
       usPoints = sumPoint(usLetter)
 
+      // com a carta comprada verifica se atigiu ou ultrapassou o limete de 21
       if (usPoints >= 21) break
 
    }
-   // compra de cartas do computador
-   while (true) {
-      if (usPoints>21 || pcPoints>=usPoints || pcPoints>=21) break
 
+   // compra de cartas (computador)
+   while (true) {
+
+      // o computador apela d+++
+      // se o usuário extrapolou os 21 pontos, ou os pontos do computador forem maiores
+      // do que os pontos do usuário ou o computador venha extrapolar os 21
+      // encerra a compra de cartas
+      if (usPoints > 21 || pcPoints >= usPoints || pcPoints >= 21) {
+         // soma dos pontos (computador)
+         pcPoints = sumPoint(pcLetter)
+         break
+      }
+
+      // computador compra uma carta
       pcLetter.push(comprarCarta())
+
+      // soma dos pontos
       pcPoints = sumPoint(pcLetter)
 
    }
 
+   // monta o texto final com as cartas compradas por ambos, mais os pontos.
    const text = resum()
 
-   if (pcPoints === usPoints){
-      alert(text+"\n"+"Houve um empate!")    
-   } else if (pcPoints > 21 || usPoints > pcPoints){
-      alert(text+"\n"+"o Usuário Ganhou!")
-   } else {
-      alert(text+"\n"+"o Computador Ganhou!")
+   if (pcPoints === usPoints) {
+      alert(text + "\n" + "Houve um empate!")
    }
 
-
-} else {
-   console.log("O jogo acabou!")
+   if (usPoints > pcPoints) {
+      if (usPoints > 21) {
+         alert(text + "\n" + "o Computador Ganhou!")
+      } else {
+         alert(text + "\n" + "o Usuário Ganhou!")
+      }
+   } else if (pcPoints > usPoints) {
+      if (pcPoints > 21) {
+         alert(text + "\n" + "o Usuário Ganhou!")
+      } else {
+         alert(text + "\n" + "o Computador Ganhou!")
+      }
+   }
 }
 
+//// ZERAR CARTAS E PONTOS
+function resetGame() {
+   usLetter = ["",""]
+   pcLetter = ["",""]
+   usPoints = [0]
+   pcPoints = [0]
+}
 
 /// RESUMO DAS CARTAS E PONTOS
-function resum(){
-   const text = "Usuário Cartas - "+letters(usLetter)+" - "+usPoints+"\n"+
-               "Computador Cartas -"+letters(pcLetter)+" - "+pcPoints+"\n"
+// monta o texto apresentado no final do game
+function resum() {
+   const text = "Usuário Cartas - " + letters(usLetter) + " - " + usPoints + "\n" +
+      "Computador Cartas -" + letters(pcLetter) + " - " + pcPoints + "\n"
    return text
 }
 
-/// CARTAS COMPRADAS
+/// retorna lista de CARTAS COMPRADAS
 function letters(arr) {
    let result = ""
    arr.forEach(obj => {
@@ -86,13 +119,10 @@ function letters(arr) {
    return result.trim()
 }
 
-/// SOMA DOS PONTOS
+/// retorna a SOMA DOS PONTOS (individual)
 function sumPoint(arr) {
-   console.log(arr)
    return arr.reduce((total, current) => {
       total += current.valor
       return total
    }, 0)
 }
-
-
