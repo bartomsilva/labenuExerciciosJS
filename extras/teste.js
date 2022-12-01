7559296b6202ecdd8821bd7c24274eaf9a0cf9bc
bartomsilva <bartomsilva@gmail.com>
2022-12-01 17:30:55 -0300

    feat: criada pasta desafio

--- extras/teste.js
+++ extras/teste.js
@@ -1,32 +1,9 @@
 
 
+(function(x) {
+    console.log("Função que chama ela mesma - TABUADA DO ",x)
+    for(let num = 1; num <=10 ; num++) 
+        console.log(` ${x} x ${num} = ${num*x}`)
+})(7)
 
-function meuFilter(arr, callback){
-    const arrRetorno=[]
-    for(const index in arr){
-        if( callback( arr[index] ) ){
-            arrRetorno.push( arr[ index ])
-        }
-    }
-    return arrRetorno
-}
-
-
-
-
-
-
-
-// um filter manual
-numeros = [ 2 , 4, 9, 5,12, 8, 10, 0, 2]
-let arrRetorno=[]
-for(const index in numeros){
-    if( numeros[index] %2 ===0 ) {
-        arrRetorno.push( numeros[index] )
-    }
-}
-//console.log(arrRetorno)
-
-arrRetorno = numeros.filter( e => e%2 == 0 )
-//console.log(arrRetorno)
 
