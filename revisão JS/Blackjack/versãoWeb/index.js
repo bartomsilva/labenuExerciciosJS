2c5a52f0cc4d3e345c2d5ea8e9169cf65008b948
bartomsilva <bartomsilva@gmail.com>
2022-12-01 12:11:36 -0300

    feat: blackjack web

--- revisão JS/Blackjack/versãoWeb/index.js
+++ revisão JS/Blackjack/versãoWeb/index.js
@@ -6,11 +6,53 @@ let usPoints = 0
 let pcPoints = 0
 let ncarta = 0
 
-///////////////////////
+//////////////////////
+function startGame() {
+    //element.classList.remove('new-class')
+    //element.classList.replace('old-class', 'new-class')
+
+    // remove as divs
+    document.getElementById('user').remove()
+    document.getElementById('pc').remove()
+
+    //cria div USER
+    let objWin = document.querySelector('#container')
+    let newDiv = document.createElement("div")
+    newDiv.classList.add('mesa')
+    newDiv.classList.add('flex')
+    newDiv.setAttribute('id', 'user')
+    objWin.appendChild(newDiv)
+    
+    //cria div PC
+    objWin = document.querySelector('#container')
+    newDiv = document.createElement("div")
+    newDiv.classList.add('mesa')
+    newDiv.classList.add('flex')
+    newDiv.setAttribute('id', 'pc')
+    objWin.appendChild(newDiv)
+
+    enableButtons()
+    lettersIni()  // cartas iniciais
+}
+
+//////////////////////////
+function enableButtons() {
+    let element = document.querySelector('#b1')
+    element.classList.toggle('none')
+    element = document.querySelector('#b2')
+    element.classList.toggle('none')
+    element = document.querySelector('#b3')
+    element.classList.toggle('none')
+    element = document.querySelector('#b4')
+    element.classList.toggle('none')
+}
+
 ///////////////////////
 function lettersIni() {
-    usLetter = []
-    pcLetter = []
+    usLetter = ["", ""]
+    pcLetter = ["", ""]
+    usPoints = 0
+    pcPoints = 0
     const iniPc = [""]
     while (true) {
         usLetter[0] = comprarCarta()
@@ -29,35 +71,100 @@ function lettersIni() {
 }
 
 //////////////////////
-//////////////////////
 function usNewLetter() {
     const newLetter = comprarCarta()
     const arrLetter = []
     arrLetter.push(newLetter)
     usLetter.push(newLetter)
-    usPoints += usLetter[usLetter.length - 1].valor
+    usPoints += arrLetter[0].valor
+
     showLetter("user", arrLetter)
+    if (usPoints >= 21) {
+        pcNewLetter()
+    }
+    return
 }
 
 ////////////////////////
-////////////////////////
 function pcNewLetter() {
-    const secPc =[""]
-    secPc[0]=pcLetter[1]
+    const secPc = []
+    secPc.push(pcLetter[1])
     showLetter("pc", secPc)
     while (true) {
         if (usPoints > 21 || pcPoints >= usPoints || pcPoints >= 21) {
             break
         }
-        let objPc=comprarCarta()
-        const arr=[""]
-        arr[0]=objPc 
-        pcLetter.push(objPc)
-        pcPoints += pcLetter[pcLetter.length - 1].valor
-        showLetter("pc", arr)
+        const newLetter = comprarCarta()
+        const arrLetter = []
+        arrLetter.push(newLetter)
+        pcLetter.push(newLetter)
+        pcPoints += arrLetter[0].valor
+        showLetter("pc", arrLetter)
     }
+
     result()
 }
+
+////////////////////////////////////
+function showLetter(userType, arr) {
+    let objWin = null
+    for (const index in arr) {
+        if (userType == "user") {
+            objWin = document.querySelector("#user")
+        } else {
+            objWin = document.querySelector("#pc")
+        }
+        ncarta++
+        // escopo local mas acessível aos elementos internos
+        let numLetter = null
+        let naipeLetter = null
+        let text = arr[index].texto
+        if (text.length == 3) {
+            numLetter = text[0]
+            naipeLetter = text[1]
+        } else { // 
+            numLetter = text[0] + text[1]
+            naipeLetter = text[2]
+        }
+        // div Carta
+        let newdiv = document.createElement("div")
+        newdiv.setAttribute('class', 'carta');
+        newdiv.setAttribute('id', 'c' + ncarta);
+        objWin.appendChild(newdiv)
+        // dentro da carta
+        objWin = document.querySelector("#c" + ncarta)
+        // criar três divs
+        newdiv = document.createElement("div")
+        newdiv.setAttribute('class', 'icarta carta-top')
+
+        // adicionando elementos da carta
+        newdiv.appendChild(createVisualValueLetter(naipeLetter))
+        objWin.appendChild(newdiv)
+
+        newdiv = document.createElement("div")
+        newdiv.setAttribute('class', 'icarta carta-center')
+
+        // adicionando elementos da carta
+        newdiv.appendChild(createVisualValueLetter(numLetter))
+        objWin.appendChild(newdiv)
+
+        newdiv = document.createElement("div")
+        newdiv.setAttribute('class', 'icarta carta-botton')
+
+        // adicionando elementos da carta
+        newdiv.appendChild(createVisualValueLetter(naipeLetter))
+        objWin.appendChild(newdiv)
+    }
+}
+
+// parte visual da carta ( naipes e valor )
+function createVisualValueLetter(element) {
+    let paragrafo = document.createElement("p")
+    let text = document.createTextNode(element)
+    paragrafo.appendChild(text)
+    return paragrafo
+}
+
 
 ///////////////////
 ///////////////////
@@ -75,102 +182,5 @@ function result() {
         pcPoints > 21 ? alert(`o Usuário Ganhou! Pontos: ${usPoints} x Computador: ${pcPoints}`) :
             alert(`o Computador Ganhou! Pontos: ${pcPoints} x Usuário ${usPoints}`)
     }
-}
-
-////////////////////////////////////
-function showLetter(userType, arr) {
-    let objWin = null
-    for (const index in arr) {
-        if (userType == "user") {
-            objWin = document.querySelector("#user")
-        } else {
-            objWin = document.querySelector("#pc")
-        }
-        ncarta++
-        // escopo local mas acessível aos elementos internos
-        let numLetter = null
-        let naipeLetter = null
-        let text = arr[index].texto
-        console.log(text)
-        if (text.length==3) {
-            numLetter = text[0]
-            naipeLetter = text[1]
-        } else { // 
-            numLetter = text[0]+text[1]
-            naipeLetter = text[2]
-        }
-        
-        // div Carta
-        let newdiv = document.createElement("div")
-        newdiv.setAttribute('class', 'carta');
-        newdiv.setAttribute('id', 'c' + ncarta);
-        objWin.appendChild(newdiv)
-        // dentro da carta
-        objWin = document.querySelector("#c" + ncarta)
-        // criar três divs
-        newdiv = document.createElement("div")
-        newdiv.setAttribute('class', 'icarta carta-top')
-        // criando elemento paragrafo 
-        let paragrafo = document.createElement("p")
-        // inserindo texto no pagragrafo
-        let texto = document.createTextNode(naipeLetter)
-        // aplicando o texto ao paragrafo
-        paragrafo.appendChild(texto);
-        // adicionando o novo paragrao a nova div
-        newdiv.appendChild(paragrafo)
-        objWin.appendChild(newdiv)
-
-        newdiv = document.createElement("div")
-        newdiv.setAttribute('class', 'icarta carta-center')
-        // criando elemento paragrafo 
-        paragrafo = document.createElement("p")
-        // inserindo texto no pagragrafo
-        texto = document.createTextNode(numLetter)
-        // aplicando o texto ao paragrafo
-        paragrafo.appendChild(texto);
-        // adicionando o novo paragrao a nova div
-        newdiv.appendChild(paragrafo)
-        objWin.appendChild(newdiv)
-
-        newdiv = document.createElement("div")
-        newdiv.setAttribute('class', 'icarta carta-botton')
-        // criando elemento paragrafo 
-        paragrafo = document.createElement("p")
-        // inserindo texto no pagragrafo
-        texto = document.createTextNode(naipeLetter)
-        // aplicando o texto ao paragrafo
-        paragrafo.appendChild(texto);
-        // adicionando o novo paragrao a nova div
-        newdiv.appendChild(paragrafo)
-        objWin.appendChild(newdiv)
-
-    }
-    //if (usPoints > 21 || pcPoints > 21) {
-    if (usPoints > 21 ) {
-        pcNewLetter()
-    }
-
-}
-
-
-
-//////////////////////
-function startGame() {
-    //element.classList.add('new-class')
-    //element.classList.remove('new-class')
-    //element.classList.replace('old-class', 'new-class')
-    enableButtons()
-    lettersIni()  // cartas iniciais
-}
-
-function enableButtons() {
-    let element = document.querySelector('#b1')
-    element.classList.toggle('none')
-    element = document.querySelector('#b2')
-    element.classList.toggle('none')
-    element = document.querySelector('#b3')
-    element.classList.toggle('none')
-    element = document.querySelector('#b4')
-    element.classList.toggle('none')
 
 }
